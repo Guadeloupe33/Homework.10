e9184edf55d2eb8181ce90d8896c8914cb5b34d8
Guadeloupe33 <stevenfrancius@gmail.com>
2023-09-22 08:36:30 -0400

    added back tick to the project

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const fs = require('fs');
 const inquirer = require ('inquirer');
 
 function generateLogo(text, textColor, shape, shapeColor) {
-    const svgMarkup = 
+    const svgMarkup = `
     <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
     <rect width="100%" height="100%" fill="${shapeColor}" />
     <!-- Implement shape drawing here -->
     <text x="10" y="150" fill="${textColor}" font-family="Arial" font-size="24">${text}</text>
   </svg>
-`;
-
+  `;
+  fs.writeFileSync('logo.svg', svgMarkup);
+  console.log('Generated logo.svg');
 }
